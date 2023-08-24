fafed34a19287b404d9e948070781949b4efb84c
cx33 <lne128gene980@163.com>
2023-08-24 16:48:19 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Image Segmentation
+
+WIP
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className='grid place-items-center w-full min-h-[100vh]'>
+      <div className="text-3xl font-bold underline">
+        Hello world!
+      </div>
+    </div>
+  )
+}
+
+export default App
